b7125ea82d3badc4f381c3ca85bad054a79b897e
ayushpasi28 <61745106+ayushpasi28@users.noreply.github.com>
2021-08-06 00:53:29 +0530

    Add files via upload

---
+++ Bubble.java
@@ -0,0 +1,27 @@
+package Practice;
+
+public class Bubble {
+	public static void main(String args[])
+	{
+int arr[]= {7,5,2,1};
+int n=arr.length;
+for(int i=0;i<n;i++)
+{
+	for(int j=0;j<n-1-i;j++)
+	{
+		if(arr[j]>arr[j+1])
+		{
+			int temp;
+			temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
+		}
+	}
+}
+for(int i=0;i<n;i++)
+{
+	System.out.println(arr[i]);
+}
+	}
+	
+}
